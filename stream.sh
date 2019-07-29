607059c25d8eafca0cdf074b7355e58489156171
Alexander <alexander.reyes@ffuf.de>
2019-07-29 19:04:18 +0800

    Flip video horizontally

--- stream.sh
+++ stream.sh
@@ -1,4 +1,4 @@
-sudo raspivid -n -w 720 -h 405 -fps 25 -vf -t 86400000 -b 1800000 -ih -o - \
+sudo raspivid -n -w 720 -h 405 -fps 25 -vf -hf -t 86400000 -b 1800000 -ih -o - \
              | ffmpeg -y \
              -i - \
              -c:v copy \
